aeacb3271e191059ea92a47c4dc870b55cfb0550
site-web <nansgaray@gmail.com>
2025-11-07 18:00:24 +0100

    Add event listener to animate heading on load

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 console.log('Hi please don\'t hack 😺');
 
+window.addEventListener('DOMContentLoaded', () => {
+  const heading = document.querySelector('h1');
+  heading.classList.add('animate');
+});
