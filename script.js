eb8c81a9db3f9c45466b4236991185132114772f
site-web <nansgaray@gmail.com>
2025-11-07 18:05:20 +0100

    Remove unnecessary console.log and event listener

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 console.log('Hi please don\'t hack 😺');
 
-window.addEventListener('DOMContentLoaded', () => {
-  const heading = document.querySelector('h1');
-  heading.classList.add('animate');
-});
